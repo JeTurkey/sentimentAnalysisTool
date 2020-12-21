eb890d6433486d8ad3a1090b92d73bc559e87e2b
Ray Shi <rayshi_1994@hotmail.com>
2020-12-21 21:54:29 +0800

    minor update

--- addingTagToNews_oneTime.py
+++ addingTagToNews_oneTime.py
@@ -22,40 +22,39 @@ mycursor.execute('SELECT * FROM ttd.news')
 
 result = mycursor.fetchall()
 
-sql = 'SELECT * FROM ttd.gov_dept'
+# sql = 'SELECT * FROM ttd.gov_dept'
 
-mycursor.execute(sql)
+# mycursor.execute(sql)
 
-gov_dept = mycursor.fetchall()
-print(gov_dept)
+# gov_dept = mycursor.fetchall()
 
-# Turn government department into dict
-ind_to_dept = {}
-dept_to_nick = {}
-for line in gov_dept:
-    ind_to_dept[line[1]] = line[0]
-    dept_to_nick[line[1]] = line[2].split(",")
-
+# # Turn government department into dict
+# ind_to_dept = {}
+# dept_to_nick = {}
+# for line in gov_dept:
+#     ind_to_dept[line[1]] = line[0]
+#     dept_to_nick[line[1]] = line[2].split(",")
 
 
-# building gov_news talbe
 
-for line in result:
-    gov_tag = ''
-    for nick in dept_to_nick:
-        for name in dept_to_nick[nick]:
-            if name in line[1] or name in line[4]:
-                gov_tag += nick + ','
-                break
-    try:
-        sql = 'UPDATE ttd.news SET gov_tag = %s WHERE news_id = %s'
-        val = (gov_tag[:-1], line[0])
-        mycursor.execute(sql, val)
-        mydb.commit()
-    except:
-        pass
+# # building gov_news talbe
 
-print('Government Done')
+# for line in result:
+#     gov_tag = ''
+#     for nick in dept_to_nick:
+#         for name in dept_to_nick[nick]:
+#             if name in line[1] or name in line[4]:
+#                 gov_tag += nick + ','
+#                 break
+#     try:
+#         sql = 'UPDATE ttd.news SET gov_tag = %s WHERE news_id = %s'
+#         val = (gov_tag[:-1], line[0])
+#         mycursor.execute(sql, val)
+#         mydb.commit()
+#     except:
+#         pass
+
+# print('Government Done')
 
 # 公司
 sql = 'SELECT * FROM ttd.company'
@@ -63,7 +62,6 @@ sql = 'SELECT * FROM ttd.company'
 mycursor.execute(sql)
 
 gov_dept = mycursor.fetchall()
-print(gov_dept)
 
 # Turn government department into dict
 ind_to_dept = {}
