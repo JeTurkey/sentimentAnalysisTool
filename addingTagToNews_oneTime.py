8cd82b9bc0d1a8d30d89b211425d3e2048b0d4ba
Ray Shi <rayshi_1994@hotmail.com>
2020-12-24 16:43:03 +0800

    version 1.4

--- addingTagToNews_oneTime.py
+++ addingTagToNews_oneTime.py
@@ -18,43 +18,45 @@ mydb  = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 
-mycursor.execute('SELECT * FROM ttd.news')
+mycursor.execute('SELECT * FROM ttd.news WHERE gov_tag IS NULL')
 
 result = mycursor.fetchall()
 
-# sql = 'SELECT * FROM ttd.gov_dept'
+print('Fectch all data')
 
-# mycursor.execute(sql)
+sql = 'SELECT * FROM ttd.gov_dept'
 
-# gov_dept = mycursor.fetchall()
+mycursor.execute(sql)
 
-# # Turn government department into dict
-# ind_to_dept = {}
-# dept_to_nick = {}
-# for line in gov_dept:
-#     ind_to_dept[line[1]] = line[0]
-#     dept_to_nick[line[1]] = line[2].split(",")
+gov_dept = mycursor.fetchall()
+
+# Turn government department into dict
+ind_to_dept = {}
+dept_to_nick = {}
+for line in gov_dept:
+    ind_to_dept[line[1]] = line[0]
+    dept_to_nick[line[1]] = line[2].split(",")
 
 
 
-# # building gov_news talbe
+# building gov_news talbe
 
-# for line in result:
-#     gov_tag = ''
-#     for nick in dept_to_nick:
-#         for name in dept_to_nick[nick]:
-#             if name in line[1] or name in line[4]:
-#                 gov_tag += nick + ','
-#                 break
-#     try:
-#         sql = 'UPDATE ttd.news SET gov_tag = %s WHERE news_id = %s'
-#         val = (gov_tag[:-1], line[0])
-#         mycursor.execute(sql, val)
-#         mydb.commit()
-#     except:
-#         pass
+for line in result:
+    gov_tag = ''
+    for nick in dept_to_nick:
+        for name in dept_to_nick[nick]:
+            if name in line[1] or name in line[4]:
+                gov_tag += nick + ','
+                break
+    try:
+        sql = 'UPDATE ttd.news SET gov_tag = %s WHERE news_id = %s'
+        val = (gov_tag[:-1], line[0])
+        mycursor.execute(sql, val)
+        mydb.commit()
+    except:
+        pass
 
-# print('Government Done')
+print('Government Done')
 
 # 公司
 sql = 'SELECT * FROM ttd.company'
@@ -88,6 +90,5 @@ for line in result:
         pass
 
 print('Company Done')
-                
 print('All done')
 mydb.close()
--- taggingData.py
+++ taggingData.py
@@ -86,12 +86,108 @@ def tagCom():
     print('All done')
     mydb.close()
 
+def tagGovOnNews():
+    mydb  = mysql.connector.connect(
+        host='rm-bp11g1acc24v9f69t1o.mysql.rds.aliyuncs.com',
+        user='rayshi',
+        password='Rayshi1994!',
+        database='ttd',
+        auth_plugin='mysql_native_password'
+    )
+
+    mycursor = mydb.cursor()
+    # Grab all untagged row
+    mycursor.execute('SELECT * FROM ttd.news WHERE gov_tag IS NULL')
+    result = mycursor.fetchall()
+
+    # Grab Gov tags
+    sql = 'SELECT * FROM ttd.gov_dept'
+    mycursor.execute(sql)
+    gov_dept = mycursor.fetchall()
+
+    # Turn government department into dict
+    ind_to_dept = {}
+    dept_to_nick = {}
+    for line in gov_dept:
+        ind_to_dept[line[1]] = line[0]
+        dept_to_nick[line[1]] = line[2].split(",")
+
+    # building gov_news talbe
+
+    for line in result:
+        gov_tag = ''
+        for nick in dept_to_nick:
+            for name in dept_to_nick[nick]:
+                if name in line[1] or name in line[4]:
+                    gov_tag += nick + ','
+                    break
+        try:
+            sql = 'UPDATE ttd.news SET gov_tag = %s WHERE news_id = %s'
+            val = (gov_tag[:-1], line[0])
+            mycursor.execute(sql, val)
+            mydb.commit()
+        except:
+            pass
+
+    print('All Done')
+    mydb.close()
+
+def tagComOnNews():
+    mydb  = mysql.connector.connect(
+        host='rm-bp11g1acc24v9f69t1o.mysql.rds.aliyuncs.com',
+        user='rayshi',
+        password='Rayshi1994!',
+        database='ttd',
+        auth_plugin='mysql_native_password'
+    )
+
+    mycursor = mydb.cursor()
+    # Grab all untagged row
+    mycursor.execute('SELECT * FROM ttd.news WHERE com_tag IS NULL')
+    result = mycursor.fetchall()
+
+    # Grab Gov tags
+    sql = 'SELECT * FROM ttd.company'
+    mycursor.execute(sql)
+    gov_dept = mycursor.fetchall()
+
+    # Turn government department into dict
+    ind_to_dept = {}
+    dept_to_nick = {}
+    for line in gov_dept:
+        ind_to_dept[line[1]] = line[0]
+        dept_to_nick[line[1]] = line[2].split(",")
+
+    # building gov_news talbe
+
+    for line in result:
+        gov_tag = ''
+        for nick in dept_to_nick:
+            for name in dept_to_nick[nick]:
+                if name in line[1] or name in line[4]:
+                    gov_tag += nick + ','
+                    break
+        try:
+            sql = 'UPDATE ttd.news SET com_tag = %s WHERE news_id = %s'
+            val = (gov_tag[:-1], line[0])
+            mycursor.execute(sql, val)
+            mydb.commit()
+        except:
+            pass
+
+    print('All Done')
+    mydb.close()
+
+
+
 
 def tagging():
     tagGov()
     print('tag Government Done')
     tagCom()
     print('tag Company Done')
+    tagGovOnNews()
+    print('Tag Gov On News Done')
 
 
 
@@ -105,5 +201,5 @@ if __name__ == "__main__":
         print(time.localtime())
         schedule.run_pending()
         print('This run end')
-        time.sleep(3600)
+        time.sleep(600)
     
